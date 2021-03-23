44e5dd30dee06dc8058edfdb6eb914c5fd4f5075
Tincho @ Genosha <tincho@genosha.com.ar>
2021-03-23 18:47:03 -0300

    agenda api

--- scripts/sections/home/agenda.ctrl.js
+++ scripts/sections/home/agenda.ctrl.js
@@ -6,7 +6,7 @@ genApp
     .controller('AgendaController', function($scope, $http,medService) {
         $scope.medService = medService;
         $scope.loading = true;
-        $http.get(URL_PREFIX + 'causas?limit=5000')
+        $http.get(URL_PREFIX + '/agenda')
         .then(function(response){
           
           $scope.agenda = response.data.resultado.causas ;
