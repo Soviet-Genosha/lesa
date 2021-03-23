6d632d4244e676e355400f33340c150170880544
Tincho @ Genosha <tincho@genosha.com.ar>
2021-03-23 18:55:08 -0300

    agenda api

--- scripts/sections/home/agenda.ctrl.js
+++ scripts/sections/home/agenda.ctrl.js
@@ -6,10 +6,10 @@ genApp
     .controller('AgendaController', function($scope, $http,medService) {
         $scope.medService = medService;
         $scope.loading = true;
-        $http.get(URL_PREFIX + '/agenda')
+        $http.get(URL_PREFIX + 'agenda')
         .then(function(response){
-          
-          $scope.agenda = response.data.resultado.causas ;
+          console.log(response);
+          $scope.agenda = response.data.resultado.agenda ;
           $scope.loading = false;
           console.log('ok');
         },function(e){
