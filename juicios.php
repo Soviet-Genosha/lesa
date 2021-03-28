84d9c2d8445a586f5ef32a28b05202fbf890545a
andres Snitcofsky <andres@soviet.com.ar>
2021-03-27 23:23:35 -0300

    updated for steting

--- juicios.php
+++ juicios.php
@@ -4,23 +4,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@200;400;700&amp;display=swap"
-        rel="stylesheet">
+        rel="stylesheet" type="text/css">
     <link rel="icon" href="/favicon.ico">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.15.1/d3.min.js"></script>
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     
 
-    <link rel="stylesheet" href="/styles/poncho.min.css">
-    <link rel="stylesheet" href="/styles/icono-arg.css">
-    <link rel="stylesheet" href="/markup/style.css" />
+    <link rel="stylesheet" type="text/css" href="/styles/poncho.min.css">
+    <link rel="stylesheet" type="text/css" href="/styles/icono-arg.css">
+    <link rel="stylesheet" type="text/css" href="/markup/style.css" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@300;400;500;700&display=swap"
-        rel="stylesheet">
+        rel="stylesheet" type="text/css">
 
     
 
--- markup/parts/footer.php
+++ markup/parts/footer.php
@@ -1,7 +1,37 @@
-<footer class="mt-5">
+
+
+<footer class="main-footer">
     <div class="container">
-        <div class="logo-arg-container d-flex justify-content-center justify-content-md-start">
-            <img src="/images/logo_argentina_unida-blanco.svg" alt="Argentina Unida">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <div class="region region-footer1">
+                    <section id="block-menu-menu-footer-1" class="block block-menu clearfix">
+                        <img src="/images/logo SDHNi.png" >
+                    </section>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="region region-footer2">
+                    <section id="block-menu-menu-footer-1" class="block block-menu clearfix">
+                        <h2 class="block-title h3 section-title h3 section-title">Secretaría de Derechos Humanos de la Nación
+                        </h2>
+                        <ul class="menu text-black">
+                            <li class="leaf">Av. Del Libertador 8151 - C1429BNB</li>
+                            <li class="leaf">Ciudad de Buenos Aires, Argentina</li>
+                            <li class="leaf">Espacio Memoria y Derechos Humanos (Ex ESMA)</li>
+                        </ul>
+                    </section>
+                </div>
+            </div>
         </div>
     </div>
-</footer>
+    <div class="container-fluid">
+        <div class="row sub-footer">
+            <div class="container">
+                <div class="col-sm-6 m-y-1 p-x-0"> <img class="image-responsive" width="288" height="40"
+                        src="/images/logo_argentina_unida-blanco.svg"
+                        alt="Argentina Unida"></div>
+            </div>
+        </div>
+    </div>
+</footer>           
