70106099ed6124d54b5cbcbc4e0fa284ea49f3fa
andres Snitcofsky <andres@soviet.com.ar>
2021-04-26 10:03:59 -0300

    meta no cache

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
+
     <meta name="description" content="Historias, estadísticas y agenda de los juicios orales por crímenes de lesa humanidad. Seguimiento de las audiencias en vivo. Análisis de la velocidad de los debates. Los juicios en datos.  ">
   <meta name="keywords" content="lesa humanidad, juicios de lesa humanidad, juicios por crímenes de lesa humanidad, dictadura militar, golpe de estado, terrorismo de estado, juicios orales lesa, audiencias lesa, juicios lesa, causas lesa, genocidas, memoria verdad y justicia, estadísticas lesa humanidad,datos lesa humanidad.">
   <meta name="author" content="Secretaria de Derechos Humanos de la Nación, Argentina">
